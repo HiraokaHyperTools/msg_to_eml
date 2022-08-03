5c25fab6390fc084349c28e3859dba86ef4a1080
kenjiuno <ku@digitaldolphins.jp>
2022-08-03 15:09:46 +0900

    Separate vLines.ts
    - Eliminate lib folder

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { deEncapsulateSync } from 'rtf-stream-parser';
 import MailComposer from 'nodemailer/lib/mail-composer';
 import { Buffer } from 'buffer';
 import path from 'path';
+import { convertVLines } from './vLines';
 
 function changeFileExtension(fileName: string, newExt: string): string {
   const parsed = path.parse(fileName);
@@ -48,43 +49,6 @@ function formatFrom(senderName: string, senderEmail: string): string {
   else {
     return `${senderEmail}`;
   }
-}
-
-function convertVLines(vLines: [string | string[], string | string[]][]): string {
-  function toText(vCell: string[] | string): string {
-    return Array.isArray(vCell)
-      ? vCell.join(';')
-      : vCell + ""
-  }
-
-  function vEscape(text: string): { more: string, text: string } {
-    if (text.match(/\r|\n|\t/)) {
-      return {
-        more: ";ENCODING=QUOTED-PRINTABLE",
-        text: text
-          .replace(/\t/g, "=09")
-          .replace(/\r/g, "=0D")
-          .replace(/\n/g, "=0A")
-        ,
-      };
-    }
-    else {
-      return { more: "", text };
-    }
-  }
-
-  const lines = [];
-  vLines.forEach(
-    vLine => {
-      if (vLine[1] === undefined) {
-        return;
-      }
-      const printer = vEscape(toText(vLine[1]));
-
-      lines.push(toText(vLine[0]) + printer.more + ":" + printer.text);
-    }
-  )
-  return lines.join("\n");
 }
 
 export interface MsgConverterOptions {
